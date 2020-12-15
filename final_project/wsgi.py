ccd3505c97edb66a91f33c39acc13daa88397b33
r08723058_william <teemoteemo0318@gmail.com>
2020-12-15 14:12:57 +0800

    test

--- final_project/wsgi.py
+++ final_project/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
+from dj_static import Cling # <- 加入
 
 import os
 
@@ -13,4 +14,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application()) # <- 修改
